d962b1bc433a08b949ee43f8aedd7a7e0a86383b
Slavast1990 <super.slava90@ukr.net>
2022-08-07 19:54:45 +0300

    map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,10 +4,10 @@ import { NavLink } from "react-router-dom";
 
 const DialogItem = (props) => {
     let path = '/dialogs/' + props.id
-    return ( 
+    return (
         <div className={classes.dialogs + ' ' + classes.active}>
-                <NavLink to={path}>{props.name}</NavLink>
-            </div>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
     )
 }
 
@@ -20,43 +20,37 @@ const Message = (props) => {
 const Dialogs = (props) => {
 
     let dialogsData = [
-        {id: 1, name: 'Dimych'},
-        {id: 2, name: 'Andrey'},
-        {id: 3, name: 'Sveta'},
-        {id: 4, name: 'Sasha'},
-        {id: 5, name: 'Victor'},
-        {id: 6, name: 'Valera'},
+        { id: 1, name: 'Dimych' },
+        { id: 2, name: 'Andrey' },
+        { id: 3, name: 'Sveta' },
+        { id: 4, name: 'Sasha' },
+        { id: 5, name: 'Victor' },
+        { id: 6, name: 'Valera' },
     ]
 
     let messageData = [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How is your it-kamasutra'},
-        {id: 3, message: 'Yo'},
-        {id: 4, message: 'Yo'},
-        {id: 5, message: 'Yo'},
-        {id: 6, message: 'Yo'},
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'How is your it-kamasutra?' },
+        { id: 3, message: 'Yo' },
+        { id: 4, message: 'Yo' },
+        { id: 5, message: 'Yo' },
+        { id: 6, message: 'Yo' },
     ]
-return (
-    <div className={classes.dialogs}>
-        <div className={classes.dialogsItems}>
-           <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-           <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-           <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-           <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-           <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-           <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
-       
+
+    let dialogsElements = dialogsData.map(dialog => <DialogItem name={dialog.name} id={dialog.id} />);
+
+    let messagesElements = messageData.map(message => <Message message={message.message} id={message.id} />)
+
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                {dialogsElements}
+            </div>
+            <div className={classes.messages}>
+                {messagesElements}
+            </div>
         </div>
-        <div className={classes.messages}>
-            <Message message={messageData[0].message} id={messageData[0].id} />
-            <Message message={messageData[1].message} id={messageData[1].id} />
-            <Message message={messageData[2].message} id={messageData[2].id} />
-            <Message message={messageData[3].message} id={messageData[3].id} />
-            <Message message={messageData[4].message} id={messageData[4].id} />
-            <Message message={messageData[5].message} id={messageData[5].id} />
-        </div>
-    </div>
-)
+    )
 }
 
 export default Dialogs;
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,10 +4,12 @@ import Post from './Post/Post';
 
 const MyPosts = () => {
 
-  let postData = [
+  let posts = [
     {id: 1, message: 'Hi, how are you?', likesCount: 12},
     {id: 2, message: "It's my first post", likesCount: 11},
 ]
+
+let postsElements = posts.map ( posts => <Post message={posts.message} likesCount={posts.likesCount} />)
 
   return (
     <div className={classes.postsBlock}>
@@ -21,9 +23,7 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={classes.posts}>
-        <Post message={postData[0].message} likesCount={postData[0].likesCount} />
-        <Post message={postData[1].message} likesCount={postData[1].likesCount} />        
-       
+        { postsElements }     
       </div>
     </div>)
 }
