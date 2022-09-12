faa3c35fa7f154a628947987b60b83f5e5ae904b
Slavast1990 <super.slava90@ukr.net>
2022-09-12 19:06:19 +0300

    users

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Dialogs from './components/Dialogs/Dialogs';
 import Profile from './components/Profile/Profile';
 import { Switch, Route} from "react-router-dom";
-import store from './redux/redux-store';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
+import UsersContainer from './components/Users/UsersContainer';
 
 const App = (props) => {
   return (
@@ -20,6 +19,7 @@ const App = (props) => {
 
             <Route path='/dialogs' render={ () => <DialogsContainer />} />
             <Route path='/profile' render={ () => <Profile />} />
+            <Route path='/users' render={ () => <UsersContainer />} />
           </Switch>
         </div>
       </div>
