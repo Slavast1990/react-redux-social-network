2b4e9edb0f7f07d37f11d617f4260d969a85014a
Slavast1990 <super.slava90@ukr.net>
2022-10-08 20:45:06 +0300

    local state

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -49,7 +49,7 @@ let mapStateToProps = (state) => {
 
 
 export default compose(
-    withAuthRedirect,
+    // withAuthRedirect,
     connect(mapStateToProps,
         { follow, unfollow, setCurrentPage, toggleIsFollowingInProgress, getUsers })
         )(UsersContainer);
